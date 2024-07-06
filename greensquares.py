0c39322baf5897d6f094fa214d6a06153da1f681
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-07-06 16:53:00 -0700

    Add files via upload

---
+++ greensquares.py
@@ -0,0 +1,30 @@
+from git import Repo
+import uuid
+import shutil
+import os
+import time
+import requests
+local_dir = "greensquarefarm"
+if os.path.isdir("greensquarefarm"):
+  shutil.rmtree("greensquarefarm")
+  print("deleted")
+repo = Repo.clone_from("git@github.com:CombineSoldier14/GreenSquareFarm.git", local_dir)
+update_file = "blank.txt"
+os.chdir("greensquarefarm")
+def webhook(message):
+    webhook = "https://discord.com/api/webhooks/1259264965274112080/sGRG6azweIELBZ4x4KwgjU4EepdBiCMPODAOcgU4AZYTziDBMX6cD-k_trOEhSLOMWBn"
+    useWebhook = (webhook != None)
+
+    if useWebhook:
+        requests.post(webhook, {
+            "content": "{} :green_square:".format(message)
+        })
+while True:
+  with open(f"{update_file}", "a") as f:
+      f.write("\n{}".format(str(uuid.uuid4())))
+  repo.index.add("blank.txt")
+  repo.index.commit("update the file")
+  os.system("git push")
+  webhook(message="# New Git Push\n<@951639877768863754>, your no-life shenagans are still working!")
+  time.sleep(2880 * 60)
+
